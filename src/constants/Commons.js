2e9b10cb91bb3467510c4bcb79c7e5eb76c64faf
Miguel Peralta <miguelps880309@gmail.com>
2016-04-06 17:45:27 -0500

    variable Fireref to correct database

--- src/constants/Commons.js
+++ src/constants/Commons.js
@@ -2,6 +2,5 @@
  * Created by epotignano on 08/03/16.
  */
 
-//export const FireRef = 'https://bushire.firebaseio.com/';
-export const FireRef = 'https://sweltering-inferno-171.firebaseio.com/';
+export const FireRef = 'https://bushire.firebaseio.com/';
 export const UidRef= 'mcw_uid';
